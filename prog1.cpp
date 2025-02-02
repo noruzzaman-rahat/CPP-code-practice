304625a967b3c474fe78a32b569a86376413ea50
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-02-02 09:29:24 +0600

    Add files via upload

---
+++ prog1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+
+{
+   int num1;
+   int num2;
+   int sum,substraction,multiplication;
+   int division;
+
+   num1=30;
+   num2=10;
+   sum=num1+num2;
+   substraction = num1-num2;
+   multiplication = num1*num2;
+   division = num1/num2;
+
+   cout<<"SUM = "<< sum<<endl;
+   cout<<"SUBS = "<<substraction<<endl;
+   cout<<"MULTI = "<<multiplication<<endl;
+   cout<<"Division = "<<division;
+
+
+
+    getch();
+}
