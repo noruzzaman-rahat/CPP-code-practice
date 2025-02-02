987ae4dd9804352e816afe9f15ed8fae951b5fb1
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-02-02 09:40:39 +0600

    Add files via upload

---
+++ prog2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+
+{
+   char name [50] = "Iam Noruzzaman";
+   char university[50] = "State University Of Bangladesh";
+   char home[50] = "Narsingdi";
+   int age = 21;
+
+
+   cout<<name<<endl;
+   cout<<"UNIVERSITY = "<<university<<endl;
+   cout<<"HOMEDISTRICT = "<<home<<endl;
+   cout<<"AGE = "<<age;
+
+
+
+
+    getch();
+}
+
