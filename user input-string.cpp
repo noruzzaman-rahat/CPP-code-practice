e3f39d53075aaaf995c61bc017f1f4b29b160272
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-02-02 09:56:47 +0600

    Add files via upload

---
+++ user input-string.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main ()
+{
+    char name[50];
+
+    cout<<"Enter your name: "<<endl;
+
+    cin>>name;
+
+    cout<<"Welcome : "<<name;
+
+
+
+    getch();
+}
+
+
