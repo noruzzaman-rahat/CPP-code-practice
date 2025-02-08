5da641b48ae09b35e9848462bdde6c7a2827b27b
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-02-08 23:18:24 +0600

    Add files via upload

--- 1036.cpp
+++ 1036.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+int main() {
+    double A, B, C;
+
+
+    cin >> A >> B >> C;
+
+
+    if (A == 0 || (B * B - 4 * A * C) < 0) {
+        cout << "Impossivel calcular" << endl;
+    } else {
+
+        double discriminant = B * B - 4 * A * C;
+
+        // Calculate the two roots using Bhaskara's formula
+        double R1 = (-B + sqrt(discriminant)) / (2 * A);
+        double R2 = (-B - sqrt(discriminant)) / (2 * A);
+
+
+        cout << fixed << setprecision(5);
+        cout << "R1 = " << R1 << endl;
+        cout << "R2 = " << R2 << endl;
+    }
+
+    return 0;
+}
