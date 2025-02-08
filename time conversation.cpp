8e68caf46a15faf7611fe32d1c797a3c9bff3ce1
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-02-08 18:17:53 +0600

    Add files via upload

---
+++ time conversation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+
+    int hours = N / 3600;
+    N %= 3600;
+    int minutes = N / 60;
+    int seconds = N % 60;
+
+    cout << hours << ":" << minutes << ":" << seconds << endl;
+
+    return 0;
+}
+
